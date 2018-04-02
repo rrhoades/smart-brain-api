228d075d56852da881f0e5f98b9d3cba2d187205
Ryan Rhoades <rrhoades@gmail.com>
2018-04-02 13:08:37 -0500

    Change API key

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 
 const app = new Clarifai.App({
- apiKey: 'd376039418664d009336e9b482484aca'
+ apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
