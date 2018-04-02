c51ff12c74050159a46073ef9a6eec3a8ccecb52
Ryan Rhoades <rrhoades@gmail.com>
2018-04-02 12:05:39 -0500

    Changed DB host to connectionString

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATBASE_URL,
+    connectionString : process.env.DATBASE_URL,
     ssl: true
   }
 });
